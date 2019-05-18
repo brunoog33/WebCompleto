86e0f0158fd43740ff766a4d9d170a6ebcb39ca3
brunoog33 <brunoog33@gmail.com>
2019-05-18 14:56:41 -0300

    Início da Orientação

---
+++ exercicios-js/objetos/OO.js
@@ -0,0 +1,18 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+object = {
+    valor1, 
+    valor2, 
+    valor3,
+    processamento() {
+        // ... 
+    }
+} // Chegar mais próximo do mundo real.
+
+// Objeto pode ter relação de parentesco... como herança
+
+
